e43e48f75895a48559b55ce337607b68b0ad3f77
Devang1224 <97470914+Devang1224@users.noreply.github.com>
2024-04-20 00:30:24 +0530

    fix: loading error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-   
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+      ) => {
+        config.externals.push({ canvas: 'commonjs canvas' })
+        return config
+      },
 };
 
 export default nextConfig;
