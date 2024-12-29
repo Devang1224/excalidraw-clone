d01e507feb1cad461dbae698603e86457d546597
Devang Mehra <devangmehra12@gmail.com>
2024-12-29 22:14:38 +0530

    Update exportToPdf.ts

--- lib/exportToPdf.ts
+++ lib/exportToPdf.ts
@@ -5,26 +5,22 @@ export const exportToPdf = async () => {
   
     if (!canvas) return;
   
-    // Prompt user for confirmation 
     const userConfirmation = await confirm(
       "Are you sure you want to export the canvas to a PDF?"
     );
   
     if (userConfirmation) {
-      // Use jspdf
+
       const doc = new jsPDF({
         orientation: "landscape",
         unit: "px",
         format: [canvas.width, canvas.height],
       });
   
-      // Get the canvas data url
       const data = canvas.toDataURL();
   
-      // Add the image to the pdf
       doc.addImage(data, "PNG", 0, 0, canvas.width, canvas.height);
   
-      // Download the pdf
       doc.save("canvas.pdf");
     } 
   };
